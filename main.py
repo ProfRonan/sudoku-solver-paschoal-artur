9f938e89eae3d18509346504a0f3b5f052008e87
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-06-14 18:12:19 -0300

    added code

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
-"""Main module to run the program."""
-from itertools import permutations
-
+def lista_com_elementos_repetidos(números: list[int]) -> bool:
+    números = [x for x in números if x != 0]
+    return len(números) != len(set(números))
 
 def solve_sudoku(board: list[list[int]]) -> list[list[int]]:
     """Solves the board"""
-    return board
+    célula_vazia = find_célula_vazia(board)
+    if célula_vazia is None:
+        return True
 
+    i, j = célula_vazia
 
-def lista_com_elementos_repetidos(números: list[list[int]]) -> bool:
-    return not len(números) != len(set(números))
+    for num in range(1,10):
+        if is_valid(board, i, j, num):
+            board[i][j] = num
+            if solve_sudoku(board):
+                return board
+            board[i][j] = 0
+    raise ValueError
 
 def is_valid(board: list[list[int]]) -> bool:
     """Checks if the board is valid"""
-    #1. Criar uma lista com os elementos de cada linha e ver se eles estão repetidos
     for linha in board:
-        linha_aux = [x for x in linha_aux if x != 0]
         if lista_com_elementos_repetidos(linha):
             return False
-    #1.Criar uma lista com os elementos de cada coluna e ver se eles estão repetidos
     for j in range(len(board)):
         coluna = [linha[j] for linha in board]
         if lista_com_elementos_repetidos(coluna):
             return False
-    #Criar uma lista com os elementos de cada diagonal e ver se eles estão repetidos
-    diagonal_1 = [board[0][0], board[1][1], board[2][2]]
-    diagonal_2 = [board[0][-1], board[1][-2], board[2][-3]]
-    for diagonal in [diagonal_1, diagonal_2]:
-        if lista_com_elementos_repetidos(diagonal):
-            return False
-    if len(board) != 9:
-        return False
-    return True
 
-def testa_tudo():
-    #Criar uma lista com todos os possíveis elementos
-    possibilidades = [1, 1, 1, 2, 2, 2, 3, 3, 3]
-    for tentativa in permutations(possibilidades):
-        print(tentativa)
+def find_célula_vazia(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0
+                return i, j
+    return None
+
+def is_full(tabuleiro):
+#verifica se o tabuleiro tem casas não preenchidas
+  for linha in tabuleiro:
+    for elemento in linha:
+      if elemento == 0:
+        return False
+  return True
+
+def still_can_play(possibilidades):
+    for linha in possibilidades:
+        for opções in linha:
+            if not opções:
+                return True
+    return False
